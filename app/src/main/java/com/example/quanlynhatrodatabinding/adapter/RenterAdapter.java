ac4cb835665897c91be1be3cef2fe6159a0442cd
Nguyễn Kiêm Tân <anhtanhangxom@gmail.com>
2020-09-11 20:27:59 +0700

    edit renter

--- app/src/main/java/com/example/quanlynhatrodatabinding/adapter/RenterAdapter.java
+++ app/src/main/java/com/example/quanlynhatrodatabinding/adapter/RenterAdapter.java
@@ -1,10 +1,15 @@
 package com.example.quanlynhatrodatabinding.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.library.baseAdapters.BR;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.quanlynhatrodatabinding.R;
 import com.example.quanlynhatrodatabinding.databinding.ItemRenterBinding;
 import com.example.quanlynhatrodatabinding.model.Renter;
 
@@ -13,21 +18,32 @@ import java.util.List;
 public class RenterAdapter extends RecyclerView.Adapter<RenterAdapter.ViewHolder> {
 
     List<Renter> list;
+    Context context;
+
+    public RenterAdapter(List<Renter> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new ViewHolder(DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.item_renter,parent,
+                false)
+        );
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        Renter renter = list.get(position);
+        holder.irb.setRenter(renter);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
